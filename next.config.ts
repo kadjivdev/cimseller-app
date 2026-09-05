6304a4df3a02845aae101235c423f9541b5b1198
kadjivdev <kadjivdev@gmail.com>
2026-09-05 14:01:47 +0100

    next.config.ts revu 3

--- next.config.ts
+++ next.config.ts
@@ -11,14 +11,16 @@
 // export default nextConfig
 
 const nextConfig = {
+  // experimental: {
+  //   webpackBuildWorker: false,
+  // },
   experimental: {
     webpackBuildWorker: false,
+    cpus: 1,
+    workerThreads: false,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  turbopack: {
-    root: __dirname, // 👈 force le bon répertoire
   },
 };
 
